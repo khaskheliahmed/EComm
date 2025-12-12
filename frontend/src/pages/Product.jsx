9039a5c950e06dd8a9c06669b5e1a8f379b794c1
khaskheliahmed <ahmadkhaskheli91@gmail.com>
2025-12-12 12:40:10 +0500

    product_working_done

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -9,7 +9,7 @@ const Product = () => {
    const {products,currency, } = useContext(ShopContext);
    const [productData, setProduct] = useState(false);
    const [image , setImage] =useState('');
-
+   const [sizes , setSizes ] = useState('')
    const fetchProductData = async ()=>{
          products.map((item)=>{
               if(item._id === productId){
@@ -36,7 +36,14 @@ const Product = () => {
               <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal'>
                 {
                   productData.image.map((item, index )=>(
-                    <img onClick={()=>setImage(item)} src={item} key={index}  className='w-[24%] sm:w-full  sm:mb-3 flex-shrink-0 cursor-pointer ' alt="" />
+                  <img 
+  onClick={() => setImage(item)} 
+  src={item} 
+  key={index}  
+  className='w-20 h-20 object-cover sm:w-24 sm:h-24 rounded-md mb-3 cursor-pointer'
+  alt="" 
+/>
+
                   ))
                 }
               </div>
@@ -63,12 +70,46 @@ const Product = () => {
              <div className='flex flex-col gap-4 my-8'>
               <p>Slect Size </p>
               <div className='flex gap-2 '>
-
+                   {
+                    productData.sizes.map((item,index) =>(
+                      <button onClick={()=>setSizes(item)} className={`border py-2 px-4 bg-gray-100 ${item === sizes ?  'border-orange-500' : '' } `} key={index}> {item} </button>
+                    ))
+                   }
               </div>
              </div>
+             <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700' > ADD TO CART</button>
+
+             <hr className='mt-8 sm:w-4/5' />
+             <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+              <p>100% orginal product.</p>
+              <p>Cash on delivery is availabe on this product.</p>
+              <p>Easy return snd exchange policy within 7 days </p>
+            
              </div>
+             </div>
+
+             
             
       </div>
+      {/* .............Describes & review............... */}
+               <div className='mt-20'>
+                <div className='flex '>
+                  <b className='border px-5 py-3 text-sm'>Description</b>
+                  <p className='border px-5 py-3 text-sm'>Reviwes (122) </p>
+                </div>
+                <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500 '>
+                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro voluptatum deleniti non. Iusto dolores deleniti assumenda dignissimos earum pariatur! Dolore doloribus aspernatur excepturi nihil minus, ipsam eius reprehenderit vel veniam?</p>
+                </div>
+               </div>
+             
+
+
+
+
+             {/* display latesh related */}
+
+
+             
     </div>
   ) : <div className='opacity-0'></div>
 }
