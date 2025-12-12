f96a3dfa611f39b2de5740bd16d78427b473ad23
khaskheliahmed <ahmadkhaskheli91@gmail.com>
2025-12-12 23:38:43 +0500

    product_completed

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProduct from '../components/RelatedProduct';
 
 const Product = () => {
 
@@ -107,9 +108,10 @@ const Product = () => {
 
 
              {/* display latesh related */}
-
-
              
+
+            <RelatedProduct category={productData.category} subCategory={productData.subCategory} />
+
     </div>
   ) : <div className='opacity-0'></div>
 }
