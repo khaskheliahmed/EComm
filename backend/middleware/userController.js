99d74ed0c0d23a91263e805f2ff7fe0ca84ef719
khaskheliahmed <ahmadkhaskheli91@gmail.com>
2025-12-17 21:22:56 +0500

    update

--- backend/middleware/userController.js
+++ backend/middleware/userController.js
@@ -1,3 +1,6 @@
+import userModel from "../models/userModel";
+import validator from "validator"
+
 
 
 //route for user login
@@ -14,7 +17,29 @@ const  loginUser = async (req, res) =>{
 
 const registerUser = async (req, res) => {
 
-    res.json({msg: "Register API Working "})
+    try {
+
+        const {name , email, password} = req.body;
+
+        // checking user already exists or not 
+        const exists = await userModel.findOne({email})
+        if(exists){
+            return res.json({success:false, message:"User already exists "})
+        }
+
+        //validating email format & stromg password
+
+        if(!validator.isEmail(email)){
+                    return res.json({success:false, message: "please enter a valid email"})
+        }
+        if(password.length < 8){
+            return res.json ({success:false, message:"please enter a strong password"})
+
+        }
+        
+    } catch (error) {
+        
+    }
 
 }
 //route for admin login
